775b2037524a99c5419bf76dc024d82a096ea2e3
Bryce B <bryce.boddy@gmail.com>
2020-05-03 09:40:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(attributes=nil)
     if attributes
-      attributes.each do |k,v|
-        self.send("#{k}=", v)
+      attributes.each do |a,b|
+        self.send("#{a}=", b)
       end
     end
   end
